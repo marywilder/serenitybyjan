d4f50df8b2a04f5af2fea9c47705f8474dfc818d
Mary <marywilder717@gmail.com>
2023-10-05 10:32:13 -0400

    okay reversed some stuff

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: { // add this code
     proxy: {
-      '/api': {
+      '/products': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
